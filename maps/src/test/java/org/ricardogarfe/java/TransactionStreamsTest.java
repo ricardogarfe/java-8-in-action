ba5b2436850367c2f8c73def3c29d38919a53817
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-07 17:14:26 +0100

    TransactionStreams Quiz Questions to define tests

--- maps/src/test/java/org/ricardogarfe/java/TransactionStreamsTest.java
+++ maps/src/test/java/org/ricardogarfe/java/TransactionStreamsTest.java
@@ -18,4 +18,14 @@ public class TransactionStreamsTest {
             new Transaction(mario, 2012, 700),
             new Transaction(alan, 2012, 950)
     );
+
+//    1. Find all transactions in the year 2011 and sort them by value (small to high).
+//    2. What are all the unique cities where the traders work?
+//    3. Find all traders from Cambridge and sort them by name.
+//    4. Return a string of all traders’ names sorted alphabetically.
+//    5. Are any traders based in Milan?
+//    6. Print all transactions’ values from the traders living in Cambridge.
+//    7. What’s the highest value of all the transactions?
+//    8. Find the transaction with the smallest value.
+
 }
