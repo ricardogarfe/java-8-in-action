1058e698d5a459db0dfb80e10c04890a4dd2fc0e
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-07 17:12:57 +0100

    TransactionStreams Quiz

---
+++ maps/src/main/java/org/ricardogarfe/java/TransactionStreams.java
@@ -0,0 +1,6 @@
+package org.ricardogarfe.java;
+
+public class TransactionStreams {
+
+
+}
---
+++ maps/src/test/java/org/ricardogarfe/java/TransactionStreamsTest.java
@@ -0,0 +1,21 @@
+package org.ricardogarfe.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TransactionStreamsTest {
+
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario","Milan");
+    Trader alan = new Trader("Alan","Cambridge");
+    Trader brian = new Trader("Brian","Cambridge");
+
+    List<Transaction> transactions = Arrays.asList(
+            new Transaction(brian, 2011, 300),
+            new Transaction(raoul, 2012, 1000),
+            new Transaction(raoul, 2011, 400),
+            new Transaction(mario, 2012, 710),
+            new Transaction(mario, 2012, 700),
+            new Transaction(alan, 2012, 950)
+    );
+}
