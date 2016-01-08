c517cc92245e73ffe7a7970c40bccc8cc5ebfd17
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-08 10:56:04 +0100

    TransactionStreams Quiz Sorted Transaction by Year and amount.

--- maps/src/main/java/org/ricardogarfe/java/TransactionStreams.java
+++ maps/src/main/java/org/ricardogarfe/java/TransactionStreams.java
@@ -1,6 +1,20 @@
 package org.ricardogarfe.java;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
+
 public class TransactionStreams {
 
+
+    public List<Transaction> sortTransactionsByYear(List<Transaction> transactions, Integer year) {
+
+        return transactions.stream()
+                .filter(t -> t.getYear() == year)
+                .sorted(comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+    }
 
 }
--- maps/src/test/java/org/ricardogarfe/java/TransactionStreamsTest.java
+++ maps/src/test/java/org/ricardogarfe/java/TransactionStreamsTest.java
@@ -1,14 +1,22 @@
 package org.ricardogarfe.java;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class TransactionStreamsTest {
 
+    private TransactionStreams transactionStreams = new TransactionStreams();
+
+    private static Integer FILTERED_YEAR = 2011;
+
     Trader raoul = new Trader("Raoul", "Cambridge");
-    Trader mario = new Trader("Mario","Milan");
-    Trader alan = new Trader("Alan","Cambridge");
-    Trader brian = new Trader("Brian","Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
 
     List<Transaction> transactions = Arrays.asList(
             new Transaction(brian, 2011, 300),
@@ -19,7 +27,16 @@ public class TransactionStreamsTest {
             new Transaction(alan, 2012, 950)
     );
 
-//    1. Find all transactions in the year 2011 and sort them by value (small to high).
+    //    1. Find all transactions in the year 2011 and sort them by value (small to high).
+    @Test
+    public void testSortTransactionsByYear() throws Exception {
+
+        List<Transaction> transactionSortedByYear = transactionStreams.sortTransactionsByYear(transactions, FILTERED_YEAR);
+        Assert.assertTrue("Transactions in 2011 are not correct.", transactionSortedByYear.size() == 2);
+
+        transactionSortedByYear.forEach(System.out::println);
+    }
+
 //    2. What are all the unique cities where the traders work?
 //    3. Find all traders from Cambridge and sort them by name.
 //    4. Return a string of all traders’ names sorted alphabetically.
