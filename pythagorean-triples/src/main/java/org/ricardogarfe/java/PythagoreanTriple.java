12b87dc4ae8308677e817fe73ab4299440a113fa
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-01-21 16:57:40 +0100

    Pythagorean Triples - Stream numbers Chapter

---
+++ pythagorean-triples/src/main/java/org/ricardogarfe/java/PythagoreanTriple.java
@@ -0,0 +1,13 @@
+package org.ricardogarfe.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class PythagoreanTriple
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ pythagorean-triples/src/test/java/org/ricardogarfe/java/PythagoreanTripleTest.java
@@ -0,0 +1,12 @@
+package org.ricardogarfe.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PythagoreanTripleTest {
+
+    @Test
+    public void testValidPythagoreanTriple() throws Exception {
+        Assert.assertTrue(true);
+    }
+}
