4fe9c55bddb0212209e45c27adfcdc7855273ded
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-02-05 09:43:14 +0100

    Group transactions by currency from jdk7 to jdk8

---
+++ collecting-data/src/main/java/org/ricardogarfe/java/Transaction.java
@@ -0,0 +1,17 @@
+package org.ricardogarfe.java;
+
+import java.util.Currency;
+
+public class Transaction {
+
+  public Currency getCurrency() {
+    return currency;
+  }
+
+  public void setCurrency(Currency currency) {
+    this.currency = currency;
+  }
+
+  private Currency currency;
+
+}
--- collecting-data/src/main/java/org/ricardogarfe/java/TransactionsCollector.java
+++ collecting-data/src/main/java/org/ricardogarfe/java/TransactionsCollector.java
@@ -1,4 +1,33 @@
 package org.ricardogarfe.java;
 
+import java.util.*;
+
+import static java.util.stream.Collectors.groupingBy;
+
 public class TransactionsCollector {
+
+  public Map<Currency, List<Transaction>> getTransactionsByCurrenciesJDK7(List<Transaction> transactions) {
+
+    Map<Currency, List<Transaction>> transactionsByCurrencies = new HashMap<>();
+    for (Transaction transaction :
+        transactions) {
+      Currency currency = transaction.getCurrency();
+
+      List<Transaction> transactionsForCurrency = transactionsByCurrencies.get(currency);
+
+      if (transactionsForCurrency == null) {
+        transactionsForCurrency = new ArrayList<>();
+        transactionsByCurrencies.put(currency, transactionsForCurrency);
+      }
+      transactionsForCurrency.add(transaction);
+    }
+    return transactionsByCurrencies;
+  }
+
+  public Map<Currency, List<Transaction>> getTransactionsByCurrencies(List<Transaction> transactions) {
+    Map<Currency, List<Transaction>> transactionsByCurrencies =
+        transactions.stream().collect(groupingBy(Transaction::getCurrency));
+
+    return transactionsByCurrencies;
+  }
 }
