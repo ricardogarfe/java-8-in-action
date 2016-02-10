298f95a8c0bdfe9576b7431926ffca6adb664c61
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-02-10 12:49:35 +0100

    Grouping dishes by type and categories using collector groupingBy.

--- collecting-data/src/main/java/org/ricardogarfe/java/DishesGroup.java
+++ collecting-data/src/main/java/org/ricardogarfe/java/DishesGroup.java
@@ -28,4 +28,21 @@ public class DishesGroup {
 
     return dishesByCaloricLevel;
   }
+
+  public Map<Dish.Type, Map<CaloricLevel, List<Dish>>> groupByTypeAndCaloricLevel(List<Dish> menu) {
+
+    Map<Dish.Type, Map<CaloricLevel, List<Dish>>> dishesByTypeAndCaloricLevel =
+        menu.stream().collect(
+            groupingBy(Dish::getType,
+              groupingBy(
+                dish -> {
+                  if (dish.getCalories() <= 400) return CaloricLevel.DIET;
+                  else if (dish.getCalories() <= 700) return CaloricLevel.NORMAL;
+                  else return CaloricLevel.FAT;
+                }
+              )
+        ));
+
+    return dishesByTypeAndCaloricLevel;
+  }
 }
