f2d1ecd9174f101044ef0e6a7c8c6fe2bd25d224
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-11 14:27:18 +0200

    Create filter to check any value

--- filters/src/main/java/org/ricardogarfe/java/FilterChapter.java
+++ filters/src/main/java/org/ricardogarfe/java/FilterChapter.java
@@ -1,4 +1,10 @@
 package org.ricardogarfe.java;
 
+import java.util.List;
+
 public class FilterChapter {
+
+  public boolean checkMenuVeganFriendly(List<Dish> menu) {
+    return menu.stream().anyMatch(Dish::isVegetarian);
+  }
 }
--- filters/src/test/java/org/ricardogarfe/java/FilterChapterTest.java
+++ filters/src/test/java/org/ricardogarfe/java/FilterChapterTest.java
@@ -1,10 +1,17 @@
 package org.ricardogarfe.java;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertTrue;
 
 public class FilterChapterTest {
 
-  public void testBasicFilter() throws Exception {
-    assertTrue(true);
+  private final FilterChapter filterChapter = new FilterChapter();
+  private final DishesUtil dishesUtil = new DishesUtil();
+
+  @Test
+  public void testCheckMenuVeganFriendy() throws Exception {
+    Boolean isVegan = filterChapter.checkMenuVeganFriendly(dishesUtil.getMenu());
+    assertTrue("The menu is not (somewhat) vegetarian friendly!!", isVegan);
   }
 }
