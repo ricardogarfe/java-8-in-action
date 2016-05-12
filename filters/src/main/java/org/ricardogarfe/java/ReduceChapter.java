01da94bbcf23bae19aacdee3a3d30f2277db96b1
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-12 16:31:31 +0200

    Refactor to include reduce methods in class under test

---
+++ filters/src/main/java/org/ricardogarfe/java/ReduceChapter.java
@@ -0,0 +1,16 @@
+package org.ricardogarfe.java;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+
+public class ReduceChapter {
+
+  public Integer reduceListInitialValue(List<Integer> numbers, BinaryOperator<Integer> binaryOperator) {
+    return numbers.stream().reduce(0, binaryOperator);
+  }
+
+  public Optional<Integer> reduceListWithoutInitialValue(List<Integer> numbers, BinaryOperator<Integer> binaryOperator) {
+    return numbers.stream().reduce(binaryOperator);
+  }
+}
--- filters/src/test/java/org/ricardogarfe/java/ReduceChapterTest.java
+++ filters/src/test/java/org/ricardogarfe/java/ReduceChapterTest.java
@@ -5,18 +5,20 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
 
 import static org.junit.Assert.assertEquals;
 
 public class ReduceChapterTest {
 
+  private ReduceChapter reduceChapter = new ReduceChapter();
   private static final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
   @Test
   public void testReduceListSum() throws Exception {
 
-    int productResult = numbers.stream().reduce(0, (a, b) -> a + b);
-    int productIntegerResult = numbers.stream().reduce(0, Integer::sum);
+    int productResult = reduceChapter.reduceListInitialValue(numbers, (a, b) -> a + b);
+    int productIntegerResult = reduceChapter.reduceListInitialValue(numbers, Integer::sum);;
 
     assertEquals("The sum is not correct", productResult, productIntegerResult);
   }
@@ -24,8 +26,8 @@ public class ReduceChapterTest {
   @Test
   public void testReduceWithoutInitialValue() throws Exception {
 
-    Optional<Integer> productResult = numbers.stream().reduce((a, b) -> a + b);
-    Optional<Integer> productIntegerResult = numbers.stream().reduce(Integer::sum);
+    Optional<Integer> productResult = reduceChapter.reduceListWithoutInitialValue(numbers, (a, b) -> a + b);
+    Optional<Integer> productIntegerResult = reduceChapter.reduceListWithoutInitialValue(numbers, Integer::sum);;
 
     assertEquals("The sum is not correct", productResult.isPresent(), productIntegerResult.isPresent());
   }
