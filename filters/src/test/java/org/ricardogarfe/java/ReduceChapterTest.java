8a7861b24f8c5d792503771635ed38b05cc39b13
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-12 16:22:52 +0200

    Reduce array sum Without Initial Value

--- filters/src/test/java/org/ricardogarfe/java/ReduceChapterTest.java
+++ filters/src/test/java/org/ricardogarfe/java/ReduceChapterTest.java
@@ -4,18 +4,30 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
 public class ReduceChapterTest {
 
+  private static final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
   @Test
   public void testReduceListSum() throws Exception {
-    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
     int productResult = numbers.stream().reduce(0, (a, b) -> a + b);
     int productIntegerResult = numbers.stream().reduce(0, Integer::sum);
 
     assertEquals("The sum is not correct", productResult, productIntegerResult);
   }
+
+  @Test
+  public void testReduceWithoutInitialValue() throws Exception {
+
+    Optional<Integer> productResult = numbers.stream().reduce((a, b) -> a + b);
+    Optional<Integer> productIntegerResult = numbers.stream().reduce(Integer::sum);
+
+    assertEquals("The sum is not correct", productResult.isPresent(), productIntegerResult.isPresent());
+  }
+
 }
